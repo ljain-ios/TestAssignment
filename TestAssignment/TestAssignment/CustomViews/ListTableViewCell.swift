1cf3240c201f9a0596ba38540428ddf3ccf2bb44
Lokesh Jain <lokeshjain1305@gmail.com>
2020-07-20 15:13:56 +0530

    View controller UI Setup

---
+++ TestAssignment/TestAssignment/CustomViews/ListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListTableViewCell.swift
+//  TestAssignment
+//
+//  Created by Lokesh on 20/07/20.
+//  Copyright © 2020 Lokesh. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
